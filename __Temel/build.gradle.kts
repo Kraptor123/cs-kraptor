255eedb46908dd94079bf75efa0785dea8ecfef2
Kraptor123 <kraptor123@gmail.com>
2025-05-01 21:06:56 +0300

    temelplugin

--- __Temel/build.gradle.kts
+++ __Temel/build.gradle.kts
@@ -2,7 +2,7 @@
 version = 0
 
 cloudstream {
-    authors     = listOf("keyiflerolsun")
+    authors     = listOf("kraptor")
     language    = "tr"
     description = "Setfilmizle sitemizde, donma yaşamadan Türkçe dublaj ve altyazılı filmleri ile dizileri muhteşem 1080p full HD kalitesinde izleyebilirsiniz."
 
