0b43b7f7036a3a1630a2fcf4927f5c38fbafa15c
Kraptor123 <kraptor123@gmail.com>
2025-05-09 00:14:58 +0300

    Film ekseni sitesi cevap vermediği için eklenti durumu çalışmıyora çekildi.

--- FilmEkseni/build.gradle.kts
+++ FilmEkseni/build.gradle.kts
@@ -13,7 +13,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 1 // will be 3 if unspecified
+    status  = 0 // will be 3 if unspecified
     tvTypes = listOf("Movie")
     iconUrl = "https://filmekseni.net/favicon.ico"
 }
