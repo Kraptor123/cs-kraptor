46eb084ab1c26bdb39ccfab0c2aca3b269fe0709
Kraptor123 <kraptor123@gmail.com>
2025-05-15 19:33:53 +0300

    url yanit vermiyor

--- FilmHane/build.gradle.kts
+++ FilmHane/build.gradle.kts
@@ -13,7 +13,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 1 // will be 3 if unspecified
+    status  = 0 // will be 3 if unspecified
     tvTypes = listOf("Movie, TvSeries")
     iconUrl = "https://filmhane.net/uploads/logo/original/logo-7349.webp"
 }
