599d8a6cb5d04af6bde34c71917514cf9cd1bf2b
Kraptor123 <kraptor123@gmail.com>
2025-05-21 16:59:43 +0300

    altyazi fix

--- HDFilmCehennemi/src/main/kotlin/com/keyiflerolsun/HDFilmCehennemi.kt
+++ HDFilmCehennemi/src/main/kotlin/com/keyiflerolsun/HDFilmCehennemi.kt
@@ -215,6 +215,7 @@ class HDFilmCehennemi : MainAPI() {
             ) {
                 // DSL builder kullanarak referer ve quality ayarı
                 this.referer = "${mainUrl}/"
+                this.headers = mapOf("User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Norton/124.0.0.0")
                 this.quality = Qualities.Unknown.value
             }
         )
