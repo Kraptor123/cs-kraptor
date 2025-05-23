8841491d39e9abf7cb8c514ea9b8a2bb24bae84a
Kraptor123 <89366989+Kraptor123@users.noreply.github.com>
2025-05-23 05:21:59 +0300

    Update Animex.kt

--- Animex/src/main/kotlin/com/kraptor/Animex.kt
+++ Animex/src/main/kotlin/com/kraptor/Animex.kt
@@ -86,7 +86,7 @@ class Animex : MainAPI() {
         }
 
         return if (url.contains("/film/"))
-            newMovieLoadResponse(title, url, type = TvType.AnimeMovie, url {
+            newMovieLoadResponse(title, url, type = TvType.AnimeMovie, url) {
                 this.posterUrl = poster
                 this.plot = description
                 this.year = year
