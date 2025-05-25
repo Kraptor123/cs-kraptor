3e80c91aef8aac6eb5fcf115e16676ede5d51f31
Kraptor123 <kraptor123@gmail.com>
2025-05-25 14:35:16 +0300

    guncelleme

--- FullHDFilmizlesene/build.gradle.kts
+++ FullHDFilmizlesene/build.gradle.kts
@@ -1,4 +1,4 @@
-version = 6
+version = 7
 
 cloudstream {
     authors     = listOf("keyiflerolsun")
@@ -12,7 +12,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 0 // will be 3 if unspecified
+    status  = 1 // will be 3 if unspecified
     tvTypes = listOf("Movie")
     iconUrl = "https://www.google.com/s2/favicons?domain=www.fullhdfilmizlesene.de&sz=%size%"
 }
