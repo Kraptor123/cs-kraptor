866330b2e1c4ec57919f8b20da44a2ca0e64206c
Kraptor123 <kraptor123@gmail.com>
2025-05-26 08:34:23 +0300

    yeni eklenti ve extractor

--- DiziKral/build.gradle.kts
+++ DiziKral/build.gradle.kts
@@ -1,10 +1,10 @@
 // ! Bu araç @Kraptor123 tarafından | @kekikanime için yazılmıştır.
-version = 2
+version = 1
 
 cloudstream {
     authors     = listOf("kraptor")
     language    = "tr"
-    description = "Sadece Dizi izlemenin Tek Adresin yerli yabancı dizi izlemek için dizimore.com"
+    description = "DiziKRAL - ücretsiz yabancı dizi izle, film izle, anime izle."
 
     /**
      * Status int as the following:
@@ -14,6 +14,6 @@ cloudstream {
      * 3: Beta only
     **/
     status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("TvSeries")
-    iconUrl = "view-source:https://dizimore.com/wp-content/uploads/2024/03/morefav.png"
+    tvTypes = listOf("Movie")
+    iconUrl = "https://dizikral.nl/mofy/img/192x192_.png"
 }
