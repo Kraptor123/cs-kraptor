a4c5b224c068f01083b68577524561fc91d04d8a
Kraptor123 <kraptor123@gmail.com>
2025-06-06 20:27:43 +0300

    anizm kategori guncellemesi.

--- Anizm/src/main/kotlin/com/kraptor/Anizm.kt
+++ Anizm/src/main/kotlin/com/kraptor/Anizm.kt
@@ -40,55 +40,91 @@ class Anizm : MainAPI() {
 
     // Ana Sayfa
     override val mainPage = mainPageOf(
-        "${mainUrl}/harf?harf=a" to "a",
-        "${mainUrl}/harf?harf=b" to "b",
-        "${mainUrl}/harf?harf=c" to "c",
-        "${mainUrl}/harf?harf=d" to "d",
-        "${mainUrl}/harf?harf=e" to "e",
-        "${mainUrl}/harf?harf=f" to "f",
-        "${mainUrl}/harf?harf=g" to "g",
-        "${mainUrl}/harf?harf=h" to "h",
-        "${mainUrl}/harf?harf=i" to "i",
-        "${mainUrl}/harf?harf=j" to "j",
-        "${mainUrl}/harf?harf=k" to "k",
-        "${mainUrl}/harf?harf=l" to "l",
-        "${mainUrl}/harf?harf=m" to "m",
-        "${mainUrl}/harf?harf=n" to "n",
-        "${mainUrl}/harf?harf=o" to "o",
-        "${mainUrl}/harf?harf=p" to "p",
-        "${mainUrl}/harf?harf=q" to "q",
-        "${mainUrl}/harf?harf=r" to "r",
-        "${mainUrl}/harf?harf=s" to "s",
-        "${mainUrl}/harf?harf=t" to "t",
-        "${mainUrl}/harf?harf=u" to "u",
-        "${mainUrl}/harf?harf=v" to "v",
-        "${mainUrl}/harf?harf=w" to "w",
-        "${mainUrl}/harf?harf=x" to "x",
-        "${mainUrl}/harf?harf=y" to "y",
-        "${mainUrl}/harf?harf=z" to "z"
+        "2"  to "Aksiyon",
+        "52" to "Arabalar",
+        "7"  to "Askeri",
+        "8"  to "Bilim-Kurgu",
+        "50" to "Bunama",
+        "9"  to "Büyü",
+        "49" to "Çocuklar",
+        "10" to "Dedektif",
+        "54" to "Dementia",
+        "11" to "Doğaüstü-Güçler",
+        "12" to "Dövüş",
+        "30" to "Dövüş Sanatları",
+        "4"  to "Dram",
+        "6"  to "Ecchi",
+        "56" to "Erkeklerin Aşkı",
+        "13" to "Fantastik",
+        "14" to "Gerilim",
+        "15" to "Gizem",
+        "16" to "Harem",
+        "17" to "Hazine-Avcılığı",
+        "55" to "Hentai",
+        "18" to "Josei",
+        "57" to "Kızların Aşkı",
+        "3"  to "Komedi",
+        "20" to "Korku",
+//        "51" to "Live Action",
+        "1"  to "Macera",
+        "21" to "Mecha",
+        "22" to "Movie",
+        "23" to "Müzik",
+        "24" to "Ninja",
+        "25" to "OAD - ONA - OVA",
+        "26" to "Okul",
+        "27" to "Oyun",
+        "48" to "Parodi",
+        "53" to "Polisiye",
+//        "28" to "Politik",
+        "29" to "Psikolojik",
+        "5"  to "Romantizm",
+        "47" to "Samuray",
+        "46" to "Savaş",
+        "31" to "Seinen",
+        "45" to "Şeytanlar",
+        "32" to "Shoujo",
+        "33" to "Shoujo-Ai",
+        "34" to "Shounen",
+        "35" to "Shounen-Ai",
+        "37" to "Spor",
+        "38" to "Süper-Güç",
+        "39" to "Tarihi",
+//        "40" to "Tuhaf",
+        "41" to "Uzay",
+        "42" to "Vampir",
+        "43" to "Yaoi",
+        "36" to "Yaşamdan Kesitler",
+        "44" to "Yuri",
     )
 
     // Ana Sayfa Yükleme
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val refH = mapOf(
-            "Referer" to mainUrl,
-            "User-Agent" to "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15 (Applebot/0.1; +http://www.apple.com/go/applebot)",
-            "Sec-Fetch-Site" to "same-origin",
-            "Host" to "anizm.net"
-            )
-        val document = if (page == 1) {
-            app.get(request.data, headers = refH).document
-        } else {
-            app.get("${request.data}&sayfa=$page", headers = refH).document
-        }
-        val home = document.select("a.pfull").mapNotNull { it.toMainPageResult() }
-        return newHomePageResponse(request.name, home)
+        val document =
+            app.post("${mainUrl}/tavsiyeRobotuResult",
+            headers = mapOf(
+                "X-CSRF-TOKEN" to "qi5SteotzukwLBcWpAQeka3IaqD3Bj2tpL547aEW",
+                "X-Requested-With" to "XMLHttpRequest",
+                "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0"),
+            data = mapOf(
+                "kategoriler%5B%5D" to request.data
+            ),
+            cookies = mapOf(
+                "adcash_shown_count2" to "3",
+                "XSRF-TOKEN" to "eyJpdiI6IlNRNkVZNmtoVm9CTkRZT0hBM0NsT0E9PSIsInZhbHVlIjoiUWlPZjFERkpINFZYZnNNMm5sc3hHZG1BZDNxS1U4dGFGMkUxWEdPNkE2NVpaT0ZJZW9JZ1U4Z3h1QzJMWFVqRzB2endzb1R5cmpMUFFNcUNITEtvUjA0Q2Rzbll4K2xkTjBhb3pmOWozeDhETUhxSk9JVkpoNGxqZlY0NGk4ZzAiLCJtYWMiOiI3M2U0MzllNDZiOTJmZTc0OTE2OTk1YzJiODU0NzBiNWJiYWFmOWY4NjdkYTIyNzIzNWZiOTNmY2ExMjUwYWRlIn0",
+                "anizm_session" to "eyJpdiI6Im1qc1wvdzViWVp5dVc4M3hqYVo5dU9BPT0iLCJ2YWx1ZSI6InNmVkNmTjFrZnFHZyttUXdTc2NJS2NcL0NCRFE2MlFLUlVvc2F0RmVUU3c1N2poc21QUUxLbmRFNTFLalJVa1AwTmlLSmZTTnhwbXI3STNwZ1wvSnE2d3lCbWFweUFQTkNLYWN4ZGFXRWhGM1dESXUrdUFFK1pjVzlheXJNQnBHVEwiLCJtYWMiOiIzNGRkMmU3NGMxODUxOTU3MDkzNmQ2YzFkZmI0OTI2NDg3YjYzZjdlMGViZjllYjk5ODIwMzM4NjEwOGJkNzI3In0="
+                )
+        ).document
+//        Log.d("Anizm","document = $document")
+
+        val home = document.select("div.aramaSonucItem").mapNotNull { it.toMainPageResult() }
+        return newHomePageResponse(request.name, home, hasNext = false)
     }
 
     private fun Element.toMainPageResult(): SearchResponse? {
-        val title = this.selectFirst("div.anizm_textUpper.anizm_textBold.truncateText")?.text() ?: return null
-        val href = fixUrlNull(this.attr("href")) ?: return null
-        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
+        val title = this.selectFirst("a.titleLink")?.text() ?: return null
+        val href = fixUrlNull(this.selectFirst("a.imgWrapperLink")?.attr("href")) ?: return null
+        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("data-src"))
         return newAnimeSearchResponse(title, href, TvType.Anime) {
             this.posterUrl = posterUrl
         }
