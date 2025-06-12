6298037626e16966f9dfa3a4791e0ce2d9e516b4
Kraptor123 <kraptor123@gmail.com>
2025-06-12 18:25:39 +0300

    fourpichive

--- RoketDizi/src/main/kotlin/com/nikyokki/ContentXExtractor.kt
+++ RoketDizi/src/main/kotlin/com/nikyokki/ContentXExtractor.kt
@@ -6,29 +6,7 @@ import android.util.Log
 import com.lagradost.cloudstream3.ErrorLoadingException
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.network.CloudflareKiller
 import com.lagradost.cloudstream3.utils.*
-import okhttp3.Interceptor
-import okhttp3.Response
-import org.jsoup.Jsoup
-
-
-private val cloudflareKiller by lazy { CloudflareKiller() }
-private val interceptor      by lazy { CloudflareInterceptor(cloudflareKiller) }
-
-class CloudflareInterceptor(private val cloudflareKiller: CloudflareKiller): Interceptor {
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val request  = chain.request()
-        val response = chain.proceed(request)
-        val doc      = Jsoup.parse(response.peekBody(1024 * 1024).string())
-
-        if (doc.html().contains("Just a moment")) {
-            return cloudflareKiller.intercept(chain)
-        }
-
-        return response
-    }
-}
 
 open class ContentX : ExtractorApi() {
     override val name            = "ContentX"
@@ -44,29 +22,9 @@ open class ContentX : ExtractorApi() {
         val extRef = referer ?: ""
         Log.d("Kekik_${this.name}", "url » $url")
 
-        val iSource = app.get(url, referer = extRef,  headers = mapOf("Referer" to url,
-            "DNT" to "1",
-            "Host" to "four.pichive.online",
-            "Pragma" to "no-cache",
-            "Priority" to "u=4",
-            "sec-ch-ua" to "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"137\", \"Google Chrome\";v=\"137\"",
-            "sec-ch-ua-mobile" to "?1",
-            "sec-ch-ua-platform" to "\"Android\"",
-            "Sec-Fetch-Dest" to "iframe",
-            "Sec-Fetch-Mode" to "navigate",
-            "Sec-Fetch-Site" to "cross-site",
-            "Sec-GPC" to "1",
-            "TE" to "trailers",
-            "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            "Accept-Language" to "en-US,en;q=0.5",
-            "Cache-Control" to "no-cache",
-            "Connection" to "keep-alive",
-            "Upgrade-Insecure-Requests" to "1",
-            "User-Agent" to "Mozilla/5.0 (iPad; CPU OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Tablet/15E148 Safari/604.1")
-        , interceptor = interceptor).text
-        Log.d("Kekik_${this.name}", "iSource » $iSource")
+        val iSource = app.get(url, referer = extRef).text
         val iExtract = Regex("""window\.openPlayer\('([^']+)'""").find(iSource)!!.groups[1]?.value ?: throw ErrorLoadingException("iExtract is null")
-        Log.d("Kekik_${this.name}", "iExtract » $iExtract")
+
         val subUrls = mutableSetOf<String>()
         Regex(""""file":"((?:\\\\\"|[^"])+)","label":"((?:\\\\\"|[^"])+)"""").findAll(iSource).forEach {
             val (subUrlExt, subLangExt) = it.destructured
@@ -97,7 +55,7 @@ open class ContentX : ExtractorApi() {
                 type   = ExtractorLinkType.M3U8
             ) {
                 headers = mapOf("Referer" to url,
-                    "User-Agent" to "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36")
+                    "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Norton/124.0.0.0")
                 quality = Qualities.Unknown.value
             }
         )
@@ -116,7 +74,7 @@ open class ContentX : ExtractorApi() {
                     type   = ExtractorLinkType.M3U8
                 ) {
                     headers = mapOf("Referer" to url,
-                        "User-Agent" to "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36")
+                        "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Norton/124.0.0.0")
                     quality = Qualities.Unknown.value
                 }
             )
