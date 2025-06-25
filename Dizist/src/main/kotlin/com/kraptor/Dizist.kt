81f3ca65968a7377231b37dfe22d792012ddded9
Kraptor123 <kraptor123@gmail.com>
2025-06-25 20:38:09 +0300

    yeni kategori ve arama fonksiyonu

--- Dizist/src/main/kotlin/com/kraptor/Dizist.kt
+++ Dizist/src/main/kotlin/com/kraptor/Dizist.kt
@@ -10,10 +10,13 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.network.CloudflareKiller
 import com.lagradost.cloudstream3.network.DdosGuardKiller
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import okhttp3.Interceptor
 import okhttp3.Response
 import org.json.JSONObject
 import org.jsoup.Jsoup
+import java.net.URLDecoder
 
 class Dizist : MainAPI() {
     override var mainUrl = "https://dizist.club"
@@ -23,109 +26,116 @@ class Dizist : MainAPI() {
     override val hasQuickSearch = false
     override val supportedTypes = setOf(TvType.TvSeries)
     //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
-
     override var sequentialMainPage =
         true        // * https://recloudstream.github.io/dokka/-cloudstream/com.lagradost.cloudstream3/-main-a-p-i/index.html#-2049735995%2FProperties%2F101969414
     override var sequentialMainPageDelay = 250L  // ? 0.05 saniye
     override var sequentialMainPageScrollDelay = 250L  // ? 0.05 saniye
-
     private var ddosGuardKiller = DdosGuardKiller(true)
+    private var cookieler: Map<String, String>? = null
+    private var cKey: String? = null
+    private var cValue: String? = null
+    private val initMutex = Mutex()
+    private suspend fun initSession() {
+        if (cookieler != null  && cKey != null && cValue != null) return
+        initMutex.withLock {
+            if (!cookieler.isNullOrEmpty() && cKey != null && cValue != null) return@withLock
+
+//            Log.d("kraptor_Dizist", "🔄 Oturum başlatılıyor: cookie, cKey ve cValue alınıyor")
+
+            val resp = app.get("${mainUrl}/",  timeout = 120)
+
+            cookieler      = resp.cookies
+
+            if (cookieler.isNullOrEmpty()) {
+                throw IllegalStateException("Çerezler boş olamaz")
+            }
+
+            val document = resp.document
+            cKey = document.selectFirst("input[name=cKey]")?.`val`()
+            cValue = document.selectFirst("input[name=cValue]")?.`val`()
+
+            Log.d("kraptor_Dizist", "cKey: $cKey, cValue: ${cValue}")
+        }
+    }
 
     override val mainPage = mainPageOf(
+        "${mainUrl}/" to "Yeni Eklenen Bölümler",
         "${mainUrl}/yabanci-diziler" to "Yabancı Diziler",
         "${mainUrl}/animeler" to "Animeler",
         "${mainUrl}/asyadizileri" to "Asya Dizileri",
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get("${request.data}/page/$page", interceptor = ddosGuardKiller).document
-        val home = document.select("div.poster-long.w-full").mapNotNull { it.toMainPageResult() }
+        initSession()
+        val cookies: Map<String, String> = cookieler!!
+        val document = if (request.name.contains("Yeni Eklenen Bölümler")) {
+            app.get("${request.data}", cookies = cookies, interceptor = ddosGuardKiller).document
+        }else{
+            app.get("${request.data}/page/$page", cookies = cookies, interceptor = ddosGuardKiller).document
+        }
+//        Log.d("kraptor_Dizist", "neden bos ${document.select("div.poster-xs")}")
+        val home = if (request.name.contains("Yeni Eklenen Bölümler")) {
+            document.select("div.poster-xs")
+                .mapNotNull { it.toMainPageResult() }
+        } else {
+            document.select("div.poster-long.w-full")
+                .mapNotNull { it.toMainPageResult() }
+        }
 
-        return newHomePageResponse(request.name, home)
+        val hasNext = !request.name.contains("Yeni Eklenen Bölümler")
+
+        return newHomePageResponse(request.name, home, hasNext = hasNext)
     }
 
     private fun Element.toMainPageResult(): SearchResponse? {
         val title = this.selectFirst("a")?.attr("title") ?: return null
-        val href = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null
+        val href = fixUrlNull(this.selectFirst("a")?.attr("href")?.replace("/izle/", "/dizi/")?.replace(Regex("-[0-9]+.*$"), "")) ?: return null
         val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("data-srcset")?.substringBefore(" "))
 
-        return newMovieSearchResponse(title, href, TvType.Movie) { this.posterUrl = posterUrl }
+        return newTvSeriesSearchResponse(title, href, TvType.TvSeries) { this.posterUrl = posterUrl }
     }
 
-//    override suspend fun search(query: String): List<SearchResponse> {
-//        // 1. Ana sayfayı çek, cKey/cValue al
-//        val initialDoc = app.get("$mainUrl/").document
-//        val cKey = initialDoc.selectFirst("input[name=cKey]")?.`val`()
-//            ?: throw IllegalStateException("cKey bulunamadı")
-//        Log.d("kraptor_$name", "cKey = ${cKey}")
-//        val cValue = initialDoc.selectFirst("input[name=cValue]")?.`val`()
-//            ?: throw IllegalStateException("cValue bulunamadı")
-//        Log.d("kraptor_$name", "cValue = ${cValue}")
-//
-//        val headers = mapOf(
-//            "Host" to "dizist.club",
-//            "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:139.0) Gecko/20100101 Firefox/139.0",
-//            "Accept" to "application/json, text/javascript, */*; q=0.01",
-//            "Accept-Language" to "en-US,en;q=0.5",
-//            "Referer" to "https://dizist.club",
-//            "Content-Type" to "application/x-www-form-urlencoded; charset=UTF-8",
-//            "Connection" to "keep-alive",
-//        )
-//
-//        val cookieler = app.get("$mainUrl/").cookies
-//
-//        val cookies = cookieler + mapOf(
-//            "newCkkKeyAw" to "0",
-//            "_ga" to "GA1.1.1427395850.1750624028",
-//            "_ga_Y0NFQBFM7S" to "GS2.1.s1750629980\$o3\$g1\$t1750629981\$j59\$l0\$h0"
-//        )
-//
-//        Log.d("kraptor_$name", "cookies = ${cookies}")
-//
-//        val apiResponse = app.post(
-//            "$mainUrl/bg/searchcontent", cookies = cookies, headers = headers, data = mapOf(
-//                "cKey" to cKey,
-//                "cValue" to cValue,
-//                "searchTerm" to query
-//            )
-//        )
-//
-//        Log.d("kraptor_$name", "apiResponse = ${apiResponse}")
-//
-//        // 3. JSON'u parse et
-//        val json = JSONObject(apiResponse.body.string())
-//        if (!json.optBoolean("state", false)) return emptyList()
-//        val html = json.optString("html", "")
-//
-//        // 4. HTML'i JSoup ile parse edip <li> öğelerini seç
-//        val doc = Jsoup.parse(html)
-//        return doc.select("ul.flex.flex-wrap li").mapNotNull { li ->
-//            li.toSearchResult()
-//        }
-//    }
-//
-//    // <li> için dönüşümü buraya taşıyoruz
-//    private fun Element.toSearchResult(): SearchResponse? {
-//        val a = this.selectFirst("a") ?: return null
-//        val title = a.selectFirst("span.truncate")?.text()?.trim() ?: return null
+    override suspend fun search(query: String): List<SearchResponse> {
+        // 1. Ana sayfayı çek, cKey/cValue al
+        initSession()
+        val cookies: Map<String, String> = cookieler!!
+        val apiResponse = app.post(
+            "$mainUrl/bg/searchcontent", cookies = cookies , data = mapOf(
+                "cKey"      to (cKey      ?: ""),
+                "cValue"    to (cValue    ?: ""),
+                "searchTerm" to query
+            )
+        )
+        val dataObj = JSONObject(apiResponse.text).getJSONObject("data")
+        val html     = dataObj.getString("html")
+        val doc = Jsoup.parseBodyFragment(html)
+        return doc.select("ul.flex.flex-wrap li").mapNotNull { li ->
+            li.toSearchResult()
+        }
+    }
+
+    private fun Element.toSearchResult(): SearchResponse? {
+        val a = this.selectFirst("a") ?: return null
+        val title = a.selectFirst("span.truncate")?.text()?.trim() ?: return null
 //        Log.d("kraptor_$name", "title = ${title}")
-//        val href = fixUrlNull(a.attr("href")) ?: return null
+        val href = fixUrlNull(a.attr("href")) ?: return null
 //        Log.d("kraptor_$name", "href = ${href}")
-//        // data-srcset içindeki ilk URL'i al
-//        val poster = a.selectFirst("img")?.attr("data-srcset")
-//            ?.substringBefore(" 1x")
-//            ?.trim()
-//            ?.let { fixUrlNull(it) }
+        val poster = a.selectFirst("img")?.attr("data-srcset")
+            ?.substringBefore(" 1x")
+            ?.trim()
+            ?.let { fixUrlNull(it) }
 //        Log.d("kraptor_$name", "poster = ${poster}")
-//        return newTvSeriesSearchResponse(title, href, TvType.Movie) {
-//            this.posterUrl = poster
-//        }
-//    }
-//
-//    override suspend fun quickSearch(query: String): List<SearchResponse> = search(query)
+        return newTvSeriesSearchResponse(title, href, TvType.Movie) {
+            this.posterUrl = poster
+        }
+    }
+
+    override suspend fun quickSearch(query: String): List<SearchResponse> = search(query)
 
     override suspend fun load(url: String): LoadResponse? {
-        val urlget = app.get(url, interceptor = ddosGuardKiller)
+        initSession()
+        val cookies: Map<String, String> = cookieler!!
+        val urlget = app.get(url, cookies = cookies, interceptor = ddosGuardKiller)
         val document = urlget.document
         val text = urlget.text
 
@@ -140,8 +150,8 @@ class Dizist : MainAPI() {
             ?.split(" ")?.first()?.trim()?.toIntOrNull()
         val actors = document.select("li.w-auto.md\\:w-full.flex-shrink-0").map { aktor ->
             val aktorIsim = aktor.selectFirst("p.truncate")?.text()?.trim() ?: return null
-            val aktorResim = fixUrlNull(aktor.selectFirst("img")?.attr("data-srcset"))
-            Actor(name = aktorIsim, aktorResim)
+            val aktorResim = fixUrlNull(aktor.selectFirst("img")?.attr("data-srcset"))?.substringBefore(" ")
+            Actor(name = aktorIsim, fixUrlNull(aktorResim))
         }
         val trailer = Regex("""embed\/(.*)\?rel""").find(document.html())?.groupValues?.get(1)
             ?.let { "https://www.youtube.com/embed/$it" }
@@ -197,7 +207,7 @@ class Dizist : MainAPI() {
         val href = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null
         val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("data-srcset")?.substringBefore(" "))
 
-        return newMovieSearchResponse(title, href, TvType.Movie) { this.posterUrl = posterUrl }
+        return newTvSeriesSearchResponse(title, href, TvType.Movie) { this.posterUrl = posterUrl }
     }
 
 
@@ -207,13 +217,15 @@ class Dizist : MainAPI() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
+        initSession()
+        val cookies: Map<String, String> = cookieler!!
         Log.d("kraptor_$name", "data = ${data}")
-        val document = app.get(data).document
+        val document = app.get(data, cookies = cookies).document
 
         val kaynakLinkleri = document
             .select("div.series-watch-alternatives.series-watch-alternatives-active.mb-5 li a.focus\\:outline-none")
 
-        Log.d("kraptor_$name", "Bulunan kaynak sayısı: ${kaynakLinkleri.size}")
+//        Log.d("kraptor_$name", "Bulunan kaynak sayısı: ${kaynakLinkleri.size}")
 
         // 2. Her bir linki işlemek için forEach kullan
         kaynakLinkleri.forEach { linkElem ->
