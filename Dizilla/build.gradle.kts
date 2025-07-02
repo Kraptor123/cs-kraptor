2ddfb31ed28c1f0fbe114e3778cf6b7ec260b222
Kraptor123 <kraptor123@gmail.com>
2025-07-02 21:35:03 +0300

    makatosagolsun

--- Dizilla/build.gradle.kts
+++ Dizilla/build.gradle.kts
@@ -1,4 +1,4 @@
-version = 29
+version = 30
 
 cloudstream {
     authors     = listOf("keyiflerolsun")
@@ -12,7 +12,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 0 // will be 3 if unspecified
+    status  = 1 // will be 3 if unspecified
     tvTypes = listOf("TvSeries")
     iconUrl = "https://www.google.com/s2/favicons?domain=dizilla.club&sz=%size%"
 }
